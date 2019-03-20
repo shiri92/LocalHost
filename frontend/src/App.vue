3e4656c43222161d70f0734e01a23f675eb8aae8
YoniR44 <taniratz@hotmail.com>
2019-03-20 15:32:07 +0200

    test profiles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,7 +2,8 @@
   <div id="app" class="flex flex-col">
       <nav class="main-nav flex justify-center align-center">
         <router-link to="/">Home</router-link>|
-        <router-link to="/about">About</router-link>
+        <router-link to="/about">About</router-link> |
+        <router-link to="/profiles">Profiles</router-link>
       </nav>
       <div class="main-container">
         <router-view></router-view>
--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,6 +1,7 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import Profiles from './views/Profiles.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
             // which is lazy-loaded when the route is visited.
             component: () =>
                 import ( /* webpackChunkName: "about" */ './views/About.vue')
+        },
+        {
+            path: '/profiles',
+            name: 'profiles',
+            component: Profiles
         }
     ]
 })
