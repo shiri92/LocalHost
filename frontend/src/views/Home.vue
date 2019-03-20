2d25fcbca2ffd63c13e14c2d15de6474df001a1f
YoniR44 <taniratz@hotmail.com>
2019-03-20 15:38:57 +0200

    update

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="home">
-    <img :src="img" alt="">
+    
   </div>
 </template>
 
 <script>
 
-import imgUrl from '../assets/img/logo.png'
+
 
 export default {
   name: 'home',
   data() {
     return {
-      
-      img: imgUrl
+
     }
   },
   components: {
