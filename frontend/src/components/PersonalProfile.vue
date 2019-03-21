ac329549074a3125c9485e7aa17a6ba89bd108df
YoniR44 <taniratz@hotmail.com>
2019-03-21 19:53:32 +0200

    updates

--- frontend/src/components/PersonalProfile.vue
+++ frontend/src/components/PersonalProfile.vue
@@ -2,8 +2,10 @@
   <section class="profile-container flex flex-row">
     <div class="side-profile" v-if="getCurrUser">
       <img class="profile-img" :src="getCurrUser.info.imgUrl" alt>
-      <div>{{getCurrUser.info.firstName}}, {{getCurrUser.info.lastName}}</div>
-      <div>{{getCurrUser.info.language}}</div>
+      <div class="profile-name">{{getCurrUser.info.firstName}}, {{getCurrUser.info.lastName}}</div>
+      <div class="profile-language">{{getCurrUser.info.language}}</div>
+      <hr>
+      <button class="request-btn">Send Request</button>
     </div>
     <div class="main-desc" v-if="getCurrUser">
       <nav class="profile-nav flex flex-row justify-center">
@@ -37,9 +39,7 @@ export default {
       return this.$store.getters.user;
     }
   },
-  methods: {
-    
-  },
+  methods: {},
   components: {
     ProfileAbout,
     ProfileMyHome,
@@ -55,15 +55,50 @@ export default {
 }
 
 .side-profile {
-  width: 20vw;
+  width: 30vw;
   border: 2px solid black;
   height: 80vh;
   margin: 5px;
   background-color: floralwhite;
-  padding: 5px;
+  padding: 15px;
   .profile-img {
     width: 100%;
-    border-radius: 10px;
+    height: 40%;
+    border-radius: 50%;
+  }
+  .profile-name {
+    font-size: 1.5em;
+  }
+  .profile-language {
+    font-size: 1.2em;
+    font-weight: bold;
+    margin: 5px;
+  }
+  .profile-language::first-letter {
+    text-transform: uppercase;
+  }
+  .request-btn {
+    margin: 5px;
+    background-color: #287fb8;
+    border: 1px solid #287fb8;
+    border-radius: 3px;
+    color: #fff;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 15px;
+    font-weight: bold;
+    line-height: 1.5;
+    max-width: 240px;
+    min-width: 6em;
+    padding: 8px 12px;
+    text-align: center;
+    text-decoration: none;
+    -webkit-transition: background-color 0.3s ease, border-color 0.3s ease;
+    transition: background-color 0.3s ease, border-color 0.3s ease;
+    white-space: normal;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    -webkit-font-smoothing: antialiased;
   }
 }
 
