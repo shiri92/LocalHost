a41eb888dda6acb07d1cd2c7265479bda662c3af
YoniR44 <taniratz@hotmail.com>
2019-03-27 11:36:55 +0200

    responsive and fixes

--- frontend/src/components/ProfileMyHome.vue
+++ frontend/src/components/ProfileMyHome.vue
@@ -1,41 +1,50 @@
 <template>
-  <section class="my-home" v-if="user">
+  <section class="my-home" v-if="pref">
     <div class="my-home-pref">
       <h3 class="header-box">MY HOME</h3>
       <hr style="margin: 0">
-      <div class="user-info flex flex-row">
-        <ul class="flex flex-col" style="width: 50%; margin: 0">
-          <li class="info-item">Max Number of Guests?
-            <span class="ans">{{user.maxNumOfGuests}}</span>
+      <div class="pref-info flex">
+        <ul class="pref-list flex flex-col">
+          <li class="info-item">
+            Max Number of Guests?
+            <span class="ans">{{pref.guestCapacity}}</span>
           </li>
-          <li class="info-item">Last-Minute Requests Okay?
-            <span class="ans">{{(user.isLastMinReq)? 'Yes':'No'}}</span>
+          <li class="info-item">
+            Last-Minute Requests Okay?
+            <span class="ans">{{(pref.isLastMinReq)? 'Yes':'No'}}</span>
           </li>
-          <li class="info-item">Preferred Gender to Host:
-            <span class="ans">{{user.prefGenderToHost}}</span>
+          <li class="info-item">
+            Preferred Gender to Host:
+            <span class="ans">{{pref.guestGenderPref}}</span>
           </li>
-          <li class="info-item">Kid Friendly?
-            <span class="ans">{{(user.isKidFriendly)? 'Yes':'No'}}</span>
+          <li class="info-item">
+            Kid Friendly?
+            <span class="ans">{{(pref.isKidFriendly)? 'Yes':'No'}}</span>
           </li>
-          <li class="info-item">Pet Friendly?
-            <span class="ans">{{(user.isPetFriendly)? 'Yes':'No'}}</span>
+          <li class="info-item">
+            Pet Friendly?
+            <span class="ans">{{(pref.isPetFriendly)? 'Yes':'No'}}</span>
           </li>
-          <li class="info-item" style="margin: 0">Smoking Allowed?
-            <span class="ans">{{(user.isSmokeAllowed)? 'Yes':'No'}}</span>
+          <li class="info-item" style="margin: 0">
+            Smoking Allowed?
+            <span class="ans">{{(pref.isDisabledAccessible)? 'Yes':'No'}}</span>
           </li>
         </ul>
-        <ul class="flex flex-col">
-          <li class="info-item">Has Pets?
-            <span class="ans">{{user.hasPets}}</span>
+        <ul class="pref-list flex flex-col">
+          <li class="info-item">
+            Has Pets?
+            <span class="ans">{{pref.pets}}</span>
           </li>
-          <li class="info-item">Has Children?
-            <span class="ans">{{user.hasChildren}}</span>
+          <li class="info-item">
+            Has Children?
+            <span class="ans">{{pref.children}}</span>
           </li>
-          <li class="info-item">Smoking at Home?
-            <span class="ans">{{(user.isSmoking)? 'Yes':'No'}}</span>
-          </li>
-          <li class="info-item">Wheelchair Accessible?
-            <span class="ans">{{(user.isWheelchairAccessible)? 'Yes':'No'}}</span>
+          <!-- <li class="info-item">Smoking at Home?
+            <span class="ans">{{(pref.isSmoking)? 'Yes':'No'}}</span>
+          </li>-->
+          <li class="info-item">
+            Disabled Accessible?
+            <span class="ans">{{(pref.isDisabledAccessible)? 'Yes':'No'}}</span>
           </li>
         </ul>
       </div>
@@ -45,7 +54,7 @@
 
 <script>
 export default {
-  props: ["user"]
+  props: ["pref"]
 };
 </script>
 
@@ -62,18 +71,31 @@ export default {
       font-weight: bold;
       font-size: 1em;
     }
-    .user-info {
+    .pref-info {
       padding: 20px;
+      flex-direction: row;
+      .pref-list {
+        width: 50%;
+        margin: 0;
+        .info-item {
+          display: inline-block;
+          text-align: left;
+          margin-bottom: 10px;
+          font-weight: bold;
+          .ans {
+            font-weight: 100;
+            margin-left: 10px;
+          }
+        }
+      }
     }
-  }
-  .info-item {
-    display: inline-block;
-    text-align: left;
-    margin-bottom: 10px;
-    font-weight: bold;
-    .ans {
-      font-weight: 100;
-      margin-left: 10px;
+    @media (max-width: 768px) {
+      .pref-info {
+        flex-direction: column;
+        .pref-list {
+          width: unset;
+        }
+      }
     }
   }
 }
